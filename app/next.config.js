beb19247b3e27b9673df4ccc34375c1504836165
Sarvar Tojikulov <sarvar.tojikulov@trueinnovation.de>
2022-09-03 22:04:24 +0200

    add next analyzer

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+/* eslint-disable */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   pageExtensions: ['ts', 'tsx'],
   experimental: {
     externalDir: true,
   },
-};
+});
