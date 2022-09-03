54490287d50962a9da80cce4eee31b7f37adf7a1
Sarvar Tojikulov <sarvar.tojikulov@trueinnovation.de>
2022-09-03 16:32:30 +0200

    add: tailwind + daisyui

--- packages/core/layout/index.tsx
+++ packages/core/layout/index.tsx
@@ -1,18 +1,18 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 
-type LayoutProps = {
-  topBar: ReactNode;
-  sidebar: ReactNode;
-  page: ReactNode;
-};
-
-export const Layout = ({ topBar, sidebar, page }: LayoutProps) => {
+export const Layout = () => {
   return (
-    <div>
-      {topBar}
-      <div>
-        {sidebar}
-        <div>{page}</div>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
       </div>
     </div>
   );
