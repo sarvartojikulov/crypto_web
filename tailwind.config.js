c89672c5ad73b0e5da9675a604661f55e7c2802e
Sarvar Tojikulov <sarvar.tojikulov@trueinnovation.de>
2022-09-04 13:11:39 +0200

    add styles and headlessui

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     path.join(__dirname, './packages/**/*.{js,ts,jsx,tsx}'),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'base-100': '#3C4452',
+        'base-200': '#323847',
+        'base-300': '#2A303C',
+        'base-400': '#242932',
+        'base-500': '#3D4451',
+        'base-content': '#CFCFCF',
+        primary: '#F6D739',
+        accent: '#37CDBE',
+      },
+    },
+  },
+  daisyui: {
+    themes: false,
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
