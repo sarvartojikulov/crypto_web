bfe706e0745c0f2b56f7e077f78f493bc9b2297e
Sarvar Tojikulov <sarvar.tojikulov@trueinnovation.de>
2022-09-20 17:06:09 +0200

    demo homepage

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,25 +1,66 @@
 import React from 'react';
 
-import Accordion from '@streact/components-accordion';
 import { BoxSectionAbout } from '@streact/components-box-sections';
 import SectionBackground from '@streact/components-section-background';
 import Ticker from '@streact/components-ticker';
-import Box from '@streact/core-box';
 import Layout from '@streact/core-layout';
 import Section from '@streact/core-section';
 import { GetStaticProps, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
+const LandingSection: React.FC = () => {
+  return (
+    <Section className="container py-28 gap-y-3 md:gap-y-6">
+      <h1 className="uppercase w-full col-span-full md:col-span-5 lg:col-span-7">
+        Crypto asset exchange
+      </h1>
+      <p className="row-start-2 col-span-full md:col-span-4 lg:col-span-5 text-sm md:text-md lg:text-lg mb-8 md:mb-0">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+        id nisi.
+      </p>
+      <button className="col-span-2 row-start-3 btn btn-active btn-primary h-14">
+        Contact
+      </button>
+      <button className="col-span-2 row-start-3 btn btn-outline btn-primary h-14">
+        Calculator
+      </button>
+      <div className="col-span-full md:row-span-3 md:col-span-3 lg:col-span-5 px-10 md:px-0 lg:px-12 mt-8 md:mt-0">
+        <Ticker />
+      </div>
+    </Section>
+  );
+};
+
+const CalculatorSection: React.FC = () => {
+  return (
+    <Section className="bg-base-300">
+      <Section className="col-span-full h-96 container">
+        <h2>Calculator</h2>
+      </Section>
+    </Section>
+  );
+};
+
+const AboutSection: React.FC = () => {
+  return (
+    <Section className="relative mt-24 pb-24">
+      <Section className="col-span-full container">
+        <h2 className="col-span-full mb-6 text-4xl">About us</h2>
+        <p className="col-span-full">Some text</p>
+        <BoxSectionAbout />
+      </Section>
+      <SectionBackground className="-z-10 absolute h-[600px] md:h-80 lg:h-56 w-full bottom-40 md:top-40 lg:bottom-0" />
+    </Section>
+  );
+};
+
 const HomePage: NextPage = () => {
   return (
     <Layout title="" description="">
-      <Section className="relative pb-24">
-        <div className="col-span-full h-20"></div>
-        <Section className="col-span-full container">
-          <BoxSectionAbout />
-        </Section>
-        <SectionBackground className="-z-10 absolute h-[600px] md:h-80 lg:h-56 w-full bottom-40 md:top-40 lg:bottom-0" />
-      </Section>
+      <LandingSection />
+      <CalculatorSection />
+      <AboutSection />
     </Layout>
   );
 };
