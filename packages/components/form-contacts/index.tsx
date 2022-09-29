2762628ba4c50b552b826d91850052c83a3c83dc
Sarvar Tojikulov <sarvar.tojikulov@trueinnovation.de>
2022-09-29 19:21:19 +0200

    add: moving to react-hook-forms

--- packages/components/form-contacts/index.tsx
+++ packages/components/form-contacts/index.tsx
@@ -1,88 +1,115 @@
+/* eslint-disable no-console */
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 
-// TODO: i18n, api call, form validation
-
-const validateEmail = (email: string) => {
-  return email
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
+const contactSchema = z.object({
+  name: z.string().min(1, { message: 'Required' }),
+  email: z
+    .string()
+    .min(1, { message: 'Email is Required' })
+    .email({ message: 'Email is invalid' }),
+  phone: z.string().min(1, { message: 'Phone is Required' }),
+  message: z.string().min(1, { message: 'Please enter your message' }),
+});
 
 const FormContacts = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(contactSchema),
+  });
+
   return (
-    <div className="col-span-full lg:col-span-7 bg-base-300 rounded-lg shadow-lg py-6 px-6">
-      <Formik
-        initialValues={{ name: '', email: '', phone: '', message: '' }}
-        validate={(values) => {
-          const errors: any = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
+    <div className="col-span-full lg:col-span-7 flex items-center justify-center bg-base-300 rounded-lg shadow-lg py-6 px-6">
+      <form
+        className="w-full grid grid-cols-4 gap-x-4 gap-y-3"
+        onSubmit={handleSubmit((data) => console.log(data))}
       >
-        {({ isSubmitting }) => (
-          <Form className="grid grid-cols-4 gap-x-4 gap-y-6">
-            <div className="flex flex-col col-span-full md:col-span-2">
-              <ErrorMessage name="name" component="label" className="label" />
-              <Field
-                type="text"
-                name="name"
-                placeholder="Name"
-                className="input input-primary w-full"
-              />
-            </div>
-            <div className="col-span-full md:col-span-2">
-              <ErrorMessage name="phone" component="label" className="label" />
-              <Field
-                type="tel"
-                name="phone"
-                placeholder="Phone"
-                className="input input-primary w-full"
-              />
-            </div>
-            <div className="col-span-full">
-              <ErrorMessage name="email" component="label" className="label" />
-              <Field
-                type="email"
-                name="email"
-                placeholder="Email"
-                className="input input-primary w-full"
-              />
-            </div>
-            <div className="col-span-full">
-              <Field
-                as="textarea"
-                name="message"
-                rows="6"
-                placeholder="Message"
-                className="textarea textarea-primary w-full"
-              />
-            </div>
-            <button
-              className="btn btn-primary col-span-2 col-start-2 md:col-start-3"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
+        <div className="flex flex-col col-span-full md:col-span-2">
+          <label className="label pt-0">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Name"
+            className="input input-primary w-full"
+            {...register('name')}
+          />
+          {errors.name?.message && (
+            <label className="label pt-1 pb-0">
+              <span className="label-text-alt text-red-400">
+                {errors.name.message}
+              </span>
+            </label>
+          )}
+        </div>
+        <div className="col-span-full md:col-span-2">
+          <label className="label pt-0">
+            <span className="label-text">Phone</span>
+          </label>
+          <input
+            type="tel"
+            placeholder="Phone"
+            className="input input-primary w-full"
+            {...register('phone')}
+          />
+          {errors.phone?.message && (
+            <label className="label pt-1 pb-0">
+              <span className="label-text-alt text-red-400">
+                {errors.phone.message}
+              </span>
+            </label>
+          )}
+        </div>
+        <div className="col-span-full">
+          <label className="label pt-0">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Email"
+            className="input input-primary w-full"
+            {...register('email')}
+          />
+          {errors.email?.message && (
+            <label className="label pt-1 pb-0">
+              <span className="label-text-alt text-red-400">
+                {errors.email.message}
+              </span>
+            </label>
+          )}
+        </div>
+        <div className="col-span-full">
+          <label className="label pt-0">
+            <span className="label-text">Message</span>
+          </label>
+          <textarea
+            placeholder="Message"
+            className="textarea textarea-primary w-full"
+            rows={4}
+            {...register('message')}
+          />
+          {errors.message?.message && (
+            <label className="label pt-1 pb-0">
+              <span className="label-text-alt text-red-400">
+                {errors.message.message}
+              </span>
+            </label>
+          )}
+        </div>
+        <button
+          className="btn btn-primary col-span-2 col-start-2 md:col-start-3"
+          type="submit"
+          onClick={handleSubmit((d) => console.log(d))}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
