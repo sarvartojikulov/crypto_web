ee1aa9dc4a45a2794d4e4e8f9fdc4ba016bceabd
Sarvar Tojikulov <sarvar.tojikulov@trueinnovation.de>
2022-10-03 21:07:34 +0200

    add about page content

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { BoxSectionAbout } from '@streact/components-box-sections';
+import SectionBackground from '@streact/components-section-background';
 import Layout from '@streact/core-layout';
 import Section from '@streact/core-section';
 import { GetServerSideProps, NextPage } from 'next';
@@ -10,10 +12,34 @@ const About: NextPage = () => {
   const { locale } = useRouter();
   return (
     <Layout title="About | Crypto Asset Exchanger" description="About us">
-      <Section className="pt-10 md:pt-20">
-        <div className="col-span-full justify-self-center text-xl">
-          <h1>About {locale}</h1>
-        </div>
+      <Section className="container pt-10 md:pt-20">
+        <article className="col-span-full lg:col-span-10 lg:col-start-2 prose max-w-full prose-h1:mb-6 md:prose-h1:mb-8 prose-h2:mb-6 md:prose-h2:mb-8 prose-h2:text-xl md:prose-h2:text-3xl">
+          <h1>What makes us different?</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non
+            lacus at dui vestibulum convallis. Sed id consequat eros. Proin ut
+            accumsan mauris, id pharetra elit. Etiam porttitor dapibus eros, nec
+            imperdiet erat pellentesque ac. Vestibulum dapibus neque eu nunc
+            malesuada feugiat. Etiam nec dictum neque. Pellentesque vehicula ac
+            nulla vitae rutrum. Proin ultricies posuere sagittis. Suspendisse
+            lobortis diam ut eleifend tempus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non
+            lacus at dui vestibulum convallis. Sed id consequat eros. Proin ut
+            accumsan mauris, id pharetra elit. Etiam porttitor dapibus eros, nec
+            imperdiet erat pellentesque ac. Vestibulum dapibus neque eu nunc
+            malesuada feugiat. Etiam nec dictum neque. Pellentesque vehicula ac
+            nulla vitae rutrum. Proin ultricies posuere sagittis. Suspendisse
+            lobortis diam ut eleifend tempus.
+          </p>
+        </article>
+      </Section>
+      <Section className="relative mt-24 pb-24">
+        <Section className="col-span-full container">
+          <BoxSectionAbout />
+        </Section>
+        <SectionBackground className="-z-10 absolute h-[600px] md:h-80 lg:h-56 w-full bottom-40 md:bottom-52 lg:bottom-20" />
       </Section>
     </Layout>
   );
@@ -22,7 +48,7 @@ const About: NextPage = () => {
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale as string, ['common'])),
+      ...(await serverSideTranslations(locale as string, ['common', 'main'])),
     },
   };
 };
--- packages/components/box-sections/About.tsx
+++ packages/components/box-sections/About.tsx
@@ -5,6 +5,7 @@ import Box from '@streact/core-box';
 import classNames from 'classnames';
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 type BoxContent = {
   icon: ReactElement;
@@ -17,26 +18,27 @@ const BOXES: BoxContent[] = [
   {
     icon: <IconSecure />,
     className: 'col-span-full md:col-span-3',
-    title: 'about.boxes.secure.title',
-    body: 'about.boxes.secure.body',
+    title: 'main:about.boxes.secure.title',
+    body: 'main:about.boxes.secure.body',
   },
   {
     icon: <IconClock />,
     className: 'col-span-full md:col-span-3',
-    title: 'about.boxes.fast.title',
-    body: 'about.boxes.fast.body',
+    title: 'main:about.boxes.fast.title',
+    body: 'main:about.boxes.fast.body',
   },
   {
     icon: <IconLegal />,
     className:
       'col-span-full md:row-start-2 md:col-start-2 md:col-span-4 lg:row-start-1 lg:col-start-auto lg:col-span-3',
-    title: 'about.boxes.legal.title',
-    body: 'about.boxes.legal.body',
+    title: 'main:about.boxes.legal.title',
+    body: 'main:about.boxes.legal.body',
   },
 ];
 
 const BoxSectionAbout: React.FC = () => {
-  const { t } = useTranslation('main');
+  const { pathname } = useRouter();
+  const { t } = useTranslation(['main', 'common']);
   return (
     <div className="col-span-full lg:col-start-2 lg:col-span-10 grid grid-cols-5 md:grid-cols-6 lg:grid-cols-9 gap-x-5 gap-y-6 justify-items-center">
       {BOXES.map(({ className, icon, title, body }) => {
@@ -49,9 +51,13 @@ const BoxSectionAbout: React.FC = () => {
             <h3 className="card-title mb-4">{t(title)}</h3>
             <p className="text-sm whitespace-normal mb-3">{t(body)}</p>
             <div className="card-actions justify-end mt-auto">
-              <Link href="/about">
+              <Link href={pathname === '/' ? '/about' : '/contact'}>
                 <button className="btn btn-sm btn-primary">
-                  {t('about.cta')}
+                  {t(
+                    pathname === '/'
+                      ? 'common:button.more'
+                      : 'common:button.contact'
+                  )}
                 </button>
               </Link>
             </div>
