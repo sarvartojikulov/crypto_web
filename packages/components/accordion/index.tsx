e7d1fc6c8453c216a7c48d77168c5bd015a88c74
Sarvar Tojikulov <sarvar.tojikulov@trueinnovation.de>
2022-10-14 19:02:16 +0200

    add accordion content styles

--- packages/components/accordion/index.tsx
+++ packages/components/accordion/index.tsx
@@ -19,13 +19,13 @@ const Accordion: React.FC<AccordionProps> = ({ content }) => {
               <>
                 <Disclosure.Button
                   className={cn(
-                    'flex w-full justify-between rounded-lg bg-base-100 px-4 py-2 text-left text-md font-medium text-base-content',
+                    'flex w-full justify-between items-center rounded-lg bg-base-100 px-4 py-2 text-left text-md font-medium text-base-content space-x-2',
                     { 'text-primary': open }
                   )}
                 >
                   <span>{header}</span>
                   <IconDropdown
-                    className={cn('h-5 w-5 transition-all', {
+                    className={cn('h-5 w-5 min-w-[20px] transition-all', {
                       'rotate-180 transform stroke-primary': open,
                       'stroke-base-content': !open,
                     })}
@@ -39,7 +39,7 @@ const Accordion: React.FC<AccordionProps> = ({ content }) => {
                   leaveFrom="transform scale-100 opacity-100"
                   leaveTo="transform scale-95 opacity-0"
                 >
-                  <Disclosure.Panel className="px-4 py-2 text-md text-base-content my-4 bg-base-100 rounded-lg prose prose-ul:list-disc max-w-none leading-4">
+                  <Disclosure.Panel className="px-4 py-2 text-md text-base-content my-4 bg-base-100 rounded-lg prose prose-ul:list-disc max-w-none prose-p:leading-5 prose-p:my-0 prose-li:my-1 leading-4">
                     {parse(body)}
                   </Disclosure.Panel>
                 </Transition>
