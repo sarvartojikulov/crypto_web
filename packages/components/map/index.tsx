b7208d1dbff0870f97b1ed3b4d2cf6922e3d2e5b
Sarvar Tojikulov <sarvar3099@gmail.de>
2022-11-20 20:42:37 +0100

    feat: update map component

--- packages/components/map/index.tsx
+++ packages/components/map/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 // made in https://maps-website.com/
 
-// TODO: Change to link to google maps
-
+// };
 const Map = () => {
   return (
     <div className="p-4 col-span-full bg-base-300 rounded-xl lg:col-span-5 shadow-lg">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d41999.41634495697!2d2.31192984720446!3d48.85890599146447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2z0J_QsNGA0LjQtiwg0KTRgNCw0L3RhtC40Y8!5e0!3m2!1sru!2sde!4v1663851396099!5m2!1sru!2sde"
+        src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=Aleja%20Solidarno%C5%9Bci%20117%20lokal%20Warsaw+(KRYPTO%20SWAP)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
         width="100%"
         height="450"
         style={{ borderRadius: '12px' }}
